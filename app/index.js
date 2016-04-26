f710b462615913702e1fc3a9bc5f7c11747b13b4
lwenke01 <lwenke@gmail.com>
2016-04-25 21:22:34 -0700

    simple

--- app/index.js
+++ app/index.js
@@ -1,7 +1,63 @@
+
 'use strict';
 
-const angular = require('angular');
-const app = angular.module('ArcadeApp', []);
+var arcades = [
+  {
+    "name": "Lisa's Amazing Games",
+    "location": "Capitol Hill",
+    "hours": "Monday - Saturday 9am-5pm"
 
-require('./arcade-controller.js')(app);
-require('./games-controller.js')(app);
+  },
+  {
+    "name": "Kat's Crazy Arcade",
+    "location": "Queen Anne",
+    "hours": "Monday - Saturday 9pm-2am"
+  },
+  {
+    "name": "Get it Games",
+    "location": "BellTown",
+    "hours": "Monday - Wednesday 9pm-2am"
+  }
+];
+
+var GetArcades = React.createClass({
+  render: function (){
+    return (
+      <div>
+      <h2>{this.props.name}</h2>
+      <p><em>location:</em> {this.props.location}</p>
+      <p><em>hours:</em> {this.props.hours}</p>
+      <div></div>
+      <button onClick={this.props.onClick}>Delete Me</button>
+      </div>
+    )
+  }
+})
+
+var App = React.createClass({
+
+  deleteArcade: function (arcade) {
+    this.state.arcades.splice(this.state.arcades.indexOf(arcade), 1);
+    this.setState({arcades: this.state.arcades});
+  },
+
+  getInitialState: function (){
+    return {
+      arcades: this.props.arcades.splice(0)
+    }
+  },
+  render: function (){
+    var that = this;
+    return (
+      <div>
+      {this.state.arcades.map(function (arcade){
+        return (
+          <GetArcades onClick={that.deleteArcade.bind(null, arcade)} name={arcade.name} location={arcade.location} hours={arcade.hours}></GetArcades>
+        )
+      })}
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(<App arcades={arcades}></App>, document.body);
--- lwenke/client-server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const express = require('express');
-const app = express();
-
-app.use(express.static(__dirname + "/app"))
-.listen(8080, () => console.log('client server started on port 8080'));
